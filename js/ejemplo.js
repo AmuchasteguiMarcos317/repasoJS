b1d61a6533b947cc945173ee8b2592152295d4cc
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-08-15 14:26:26 -0300

    ejemplos motos

---
+++ js/ejemplo.js
@@ -0,0 +1,71 @@
+let moto1 = {
+    id:560392,
+    marca: 'ktm',
+    modelo: 'sxf',
+    tipo: 'motocross',
+    cilindrada:250,
+    año:2022,
+    precio: 22000,
+    imagen: 'https://www.motorevistacr.com/ktm-250-sx-f-factory-edition-2022/',
+
+}
+
+let moto2 = {
+    id:561092,
+    marca: 'yamaha',
+    modelo: 'yzf',
+    tipo: 'motocross',
+    cilindrada:250,
+    año:2021,
+    precio: 17000,
+    imagen: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.yamaha-motor.com.ar%2Fimages%2Fproductos%2Febe894d9b921c22637b4e1ba2b9f9950_md.jpg%3F666&imgrefurl=https%3A%2F%2Fwww.yamaha-motor.com.ar%2Fmodelo%2Fyz250f&tbnid=RtCFTs4KY49vGM&vet=12ahUKEwijhvu9rcn5AhUGlJUCHcd3AEYQMygCegUIARDGAQ..i&docid=qnEjL0OjEvTZCM&w=1000&h=750&q=yamaha%20yzf%20250&ved=2ahUKEwijhvu9rcn5AhUGlJUCHcd3AEYQMygCegUIARDGAQ',
+
+}
+
+let moto3 = {
+    id:961092,
+    marca: 'husqvarna',
+    modelo: 'fe',
+    tipo: 'enduro',
+    cilindrada:350,
+    año:2022,
+    precio: 22000,
+    imagen: 'https://motos0km.com.ar/fichas-tecnicas/husqvarna/fe-350/',
+
+}
+let moto4 = {
+    id:961992,
+    marca: 'kawasaki',
+    modelo: 'kxf',
+    tipo: 'motocross',
+    cilindrada:450,
+    año:2022,
+    precio: 22000,
+    imagen: 'https://www.motociclismo.es/moto-verde/kawasaki-kx-450-2020-prueba-fotos-ficha-tecnica_190213_102.html',
+
+}
+
+let moto5 = {
+    id:933092,
+    marca: 'honda',
+    modelo: 'crf',
+    tipo: 'motocross',
+    cilindrada:250,
+    año:2022,
+    precio: 15000,
+    imagen: 'https://www.motorbikemag.es/ficha-tecnica/honda-crf250r-2022/',
+
+}
+
+let moto = {
+    id:9355092,
+    marca: 'yamaha',
+    modelo: 'wr',
+    tipo: 'enduro',
+    cilindrada:250,
+    año:2022,
+    precio: 15000,
+    imagen: 'https://enduro21.com/es/motos/ultimos/primera-impresion-yamaha-wr250f-2022-actualizaciones-para-la-pequena-de-la-gama',
+
+}
+
